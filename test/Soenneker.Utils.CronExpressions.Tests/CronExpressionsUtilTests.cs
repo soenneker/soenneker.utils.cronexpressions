56e305a2ac1e9b79cf6e33a5ad794a37f7b24aca
Jake Soenneker <jake@soenneker.com>
2026-03-04 14:25:54 -0600

    Deps, tests

--- test/Soenneker.Utils.CronExpressions.Tests/CronExpressionsUtilTests.cs
+++ test/Soenneker.Utils.CronExpressions.Tests/CronExpressionsUtilTests.cs
@@ -1,4 +1,6 @@
-﻿using Soenneker.Tests.FixturedUnit;
+using System;
+using Soenneker.Enums.DayOfWeek;
+using Soenneker.Tests.FixturedUnit;
 using Xunit;
 
 namespace Soenneker.Utils.CronExpressions.Tests;
@@ -14,5 +16,32 @@ public class CronExpressionsUtilTests : FixturedUnitTest
     public void Default()
     {
 
+    }
+
+    public static TheoryData<DayOfWeekType, string> ToCronDayCases => new()
+    {
+        { DayOfWeekType.Monday, "MON" },
+        { DayOfWeekType.Tuesday, "TUE" },
+        { DayOfWeekType.Wednesday, "WED" },
+        { DayOfWeekType.Thursday, "THU" },
+        { DayOfWeekType.Friday, "FRI" },
+        { DayOfWeekType.Saturday, "SAT" },
+        { DayOfWeekType.Sunday, "SUN" },
+    };
+
+    [Theory]
+    [MemberData(nameof(ToCronDayCases))]
+    public void ToCronDay_returns_expected_abbreviation(DayOfWeekType day, string expected)
+    {
+        string result = CronExpressionUtil.ToCronDay(day);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void ToCronDay_invalid_throws_ArgumentOutOfRangeException()
+    {
+        var invalidDay = (DayOfWeekType)null!;
+        var ex = Assert.Throws<ArgumentOutOfRangeException>(() => CronExpressionUtil.ToCronDay(invalidDay));
+        Assert.Equal("day", ex.ParamName);
     }
 }
